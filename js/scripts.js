9d02859b85e3d039676e9e4a3e05cc7ca7ab2b97
Matthew <78713317+mreed4@users.noreply.github.com>
2022-03-31 14:28:56 -0700

    begin adding CSS, styling, fonts, etc.

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,10 @@
 let calcDisplay = document.getElementById("display");
-calcDisplay.innerHTML = "Test";
+
+const operate = (a, b, operator) => {
+  return {
+    "+": a + b,
+    "-": a - b,
+    "/": a / b,
+    "*": a * b,
+  }[operator];
+};
