bdfedd2124d83a8eada71f6e9838b91c5c78edb3
Matthew <78713317+mreed4@users.noreply.github.com>
2022-03-31 21:58:54 -0700

    SOME buttons now 'work'; Display works, sorta

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,27 @@
-let calcDisplay = document.getElementById("display");
-let a, b;
+let numbers = document.getElementById("numbers");
+let numberButtons = document.getElementsByClassName("btn-number");
+let operatorButtons = document.getElementsByClassName("btn-operator");
 
-const operate = (a, b, operator) => {
+function getFirstNumber() {
+  [...numberButtons].forEach((button) => {
+    button.addEventListener("click", () => {
+      if (numbers.innerText.length < 8) {
+        numbers.innerText += button.innerText;
+        console.log(numbers.innerText.length);
+      }
+    });
+  });
+  return numbers.innerText;
+}
+
+function OPERATE(a, b, operator) {
   return {
     "+": a + b,
     "-": a - b,
     "/": a / b,
     "*": a * b,
+    "%": a % b,
   }[operator];
-};
+}
+
+getFirstNumber();
