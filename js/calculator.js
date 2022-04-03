6de9f6f9bf1dd2e423b5c6d087f0d65fe58ae679
Matthew <78713317+mreed4@users.noreply.github.com>
2022-04-02 17:45:43 -0700

    Buttons connected to display

--- js/calculator.js
+++ js/calculator.js
@@ -1,20 +1,36 @@
 let numbers = document.getElementById("numbers");
 let numberButtons = document.getElementsByClassName("btn-number");
 let operatorButtons = document.getElementsByClassName("btn-operator");
+let clear = document.getElementsByClassName("btn-clear")[0];
 
 function getFirstNumber() {
   [...numberButtons].forEach((button) => {
     button.addEventListener("click", () => {
       if (numbers.innerText.length < 8) {
         numbers.innerText += button.innerText;
-        console.log(numbers.innerText.length);
+      }
+      if (numbers.innerText.length === 8) {
+        [...numberButtons].map((button) => {
+          button.classList.remove("btn-enabled");
+          button.classList.add("btn-disabled");
+        });
       }
     });
   });
   return numbers.innerText;
 }
 
-function OPERATE(a, b, operator) {
+function clearScreen() {
+  clear.addEventListener("click", () => {
+    numbers.innerText = "";
+    [...numberButtons].forEach((button) => {
+      button.classList.remove("btn-disabled");
+      button.classList.add("btn-enabled");
+    });
+  });
+}
+
+function operate(a, b, operator) {
   return {
     "+": a + b,
     "-": a - b,
@@ -25,3 +41,4 @@ function OPERATE(a, b, operator) {
 }
 
 getFirstNumber();
+clearScreen();
